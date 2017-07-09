67abcf08b2c5a338c6b6c5e08461caf65146a1a6
Andrey Eltsov <andrei.eltzov@gmail.com>
2017-07-09 13:38:09 +0300

    Добавлены прототипы главного меню и внутриигрового интерфйса
    -Добавлены прототипы главного меню и внутриигрового интерфйса
    -Добавлены спрайты связанные с интерфейсом
    
    Автор изменений: Алидар Асваров

--- Strategy/Assets/Scripts/Map.cs
+++ Strategy/Assets/Scripts/Map.cs
@@ -7,17 +7,56 @@ public class Map : MonoBehaviour {
     public int NumberOfCellsOnAxisY = 10;
     public GameObject CellPrefab;
 
+
     void Start () {
         GenerateNewTable();
-
+       // OnMouseEnter();
     }
-	
 
-	void Update () {
-	
-	}
-    public void GenerateNewTable()
+
+    void FixedUpdate () {
+#if UNITY_STANDALONE_WIN
+        if (Input.GetMouseButtonDown(0))
+#endif
+//#if UNITY_ANDROID
+        for (int i = 0; i < Input.touchCount; ++i)
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+                ScreenRay(i);
+            }
+                
+//#endif
+            
+           
+        //if (Input.GetAxis("Cancel") > 0)
+        //{
+         //   OnMouseExit();
+        //}
+    }
+
+
+
+    public void ScreenRay(int androidI)
     {
+        RaycastHit2D hitInfo = new RaycastHit2D();
+
+#if UNITY_STANDALONE_WIN
+        hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+#endif
+
+#if UNITY_ANDROID
+        hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.GetTouch(androidI).position), Vector2.zero);
+       // hitInfo = Physics2D.Raycast(new Vector2 (-4.249481f, -1.17f), Vector2.zero);
+#endif
+
+        if (hitInfo.collider)
+        {
+            Debug.Log(hitInfo.transform.gameObject.name);
+        }
+    }
+
+        public void GenerateNewTable()
+        {
         for (int i = 0; i < NumberOfCellsOnAxisX; i++)
         {
             GameObject Line = new GameObject();
@@ -30,7 +69,23 @@ public class Map : MonoBehaviour {
                 newCell.transform.parent = Line.transform;
                 newCell.transform.position = transform.position + new Vector3(i, j);
                 newCell.name = "Cell_" + i + "_" + j;
+
+
             }
         }
+    }
+
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
     }
 }
