ba4d11c3c24fc00488b6475def418bedff302865
Andrey Eltsov <andrei.eltzov@gmail.com>
2017-07-11 22:31:06 +0300

    Добавлено меню.
    Добавлено меню.

---
+++ Strategy/Assets/Scripts/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour
+{
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void Singleplayer()
+    {
+        SceneManager.LoadScene("TestScen", LoadSceneMode.Single);
+    }
+}
