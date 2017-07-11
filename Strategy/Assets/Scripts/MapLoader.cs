f68a8fc0f8f380e9dd45892948b259957d396b8e
Andrey Eltsov <andrei.eltzov@gmail.com>
2017-07-11 17:25:02 +0300

    Добавлен save/load.
    Добавлен save/load.

--- Strategy/Assets/Scripts/MapLoader.cs
+++ Strategy/Assets/Scripts/MapLoader.cs
@@ -10,6 +10,7 @@ public class MapLoader : MonoBehaviour
 {
     Map MapToSave;
     Unit UnitBufer;
+    Construction ConstructionUnit;
     Cell CellBufer;
     Camera CameraToSave;
     GameObject mapPrefab;
@@ -25,35 +26,55 @@ public class MapLoader : MonoBehaviour
     {
         StreamWriter sw = new StreamWriter(saveName + ".txt");
 
-        sw.WriteLine("CameraSettings");
+        /*sw.WriteLine("CameraSettings");
         sw.WriteLine(CameraToSave.transform.position.x);
         sw.WriteLine(CameraToSave.transform.position.y);
         sw.WriteLine(CameraToSave.transform.position.z);
 
         sw.WriteLine("MapSettings");
         sw.WriteLine(MapToSave.NumberOfCellsOnAxisX);
-        sw.WriteLine(MapToSave.NumberOfCellsOnAxisY);
+        sw.WriteLine(MapToSave.NumberOfCellsOnAxisY);*/
 
-        for (int i = 0; i < MapToSave.NumberOfCellsOnAxisX; i++)
-        {
-            for (int j = 0; j < MapToSave.NumberOfCellsOnAxisY; j++)
-            {
-                sw.WriteLine(MapToSave.GetCell(i, j).Type);
-            }
-        }
+        //for (int i = 0; i < MapToSave.NumberOfCellsOnAxisX; i++)
+        //{
+        //    for (int j = 0; j < MapToSave.NumberOfCellsOnAxisY; j++)
+        //    {
+        //        sw.WriteLine(MapToSave.GetCell(i, j).Type);
+        //    }
+        //}
 
         sw.WriteLine("UnitsSettings");
         sw.WriteLine(MapToSave.UnitList.Count);
         for (int i = 0; i < MapToSave.UnitList.Count; i++)
         {
-            sw.WriteLine(MapToSave.UnitList[i].Type);
-            sw.WriteLine(MapToSave.UnitList[i].name);
-            sw.WriteLine(MapToSave.UnitList[i].CurrentCell.indexX);
-            sw.WriteLine(MapToSave.UnitList[i].CurrentCell.indexY);
-            sw.WriteLine(MapToSave.UnitList[i].Hitpoints);
-            sw.WriteLine(MapToSave.UnitList[i].Damage);
-            sw.WriteLine(MapToSave.UnitList[i].CurrentNumberActionPoints);
-            sw.WriteLine(MapToSave.UnitList[i].StandardNumberActionPoints);
+            if (MapToSave.UnitList[i].Type == Unit.UnitType.Construction)
+            {
+                sw.WriteLine("Construction");
+                ConstructionUnit = GameObject.Find(MapToSave.UnitList[i].name).GetComponent<Construction>();
+                sw.WriteLine(ConstructionUnit._ConstructionType);
+                sw.WriteLine(ConstructionUnit.name);
+                sw.WriteLine(ConstructionUnit.CurrentCell.indexX);
+                sw.WriteLine(ConstructionUnit.CurrentCell.indexY);
+                sw.WriteLine(ConstructionUnit.Fraction);
+                sw.WriteLine(ConstructionUnit.Hitpoints);
+                sw.WriteLine(ConstructionUnit.Damage);
+                sw.WriteLine(ConstructionUnit.CurrentNumberActionPoints);
+                sw.WriteLine(ConstructionUnit.StandardNumberActionPoints);
+
+            }
+            else
+            {
+                sw.WriteLine("Unit");
+                sw.WriteLine(MapToSave.UnitList[i].Type);
+                sw.WriteLine(MapToSave.UnitList[i].name);
+                sw.WriteLine(MapToSave.UnitList[i].CurrentCell.indexX);
+                sw.WriteLine(MapToSave.UnitList[i].CurrentCell.indexY);
+                sw.WriteLine(MapToSave.UnitList[i].Fraction);
+                sw.WriteLine(MapToSave.UnitList[i].Hitpoints);
+                sw.WriteLine(MapToSave.UnitList[i].Damage);
+                sw.WriteLine(MapToSave.UnitList[i].CurrentNumberActionPoints);
+                sw.WriteLine(MapToSave.UnitList[i].StandardNumberActionPoints);
+            }
 
         }
 
@@ -71,7 +92,7 @@ public class MapLoader : MonoBehaviour
             StreamReader sr = new StreamReader(saveName + ".txt");
             string buf;
             buf = sr.ReadLine();
-            Vector3 newCamPos = new Vector3(Convert.ToSingle(sr.ReadLine()), Convert.ToSingle(sr.ReadLine()), Convert.ToSingle(sr.ReadLine()));
+            /*Vector3 newCamPos = new Vector3(Convert.ToSingle(sr.ReadLine()), Convert.ToSingle(sr.ReadLine()), Convert.ToSingle(sr.ReadLine()));
             CameraToSave.transform.position = newCamPos;
             buf = sr.ReadLine();
             buf = sr.ReadLine();
@@ -106,39 +127,72 @@ public class MapLoader : MonoBehaviour
                         CellBufer.SetType(5);
                     }
                 }
-            }
+            }*/
 
-            buf = sr.ReadLine();
+            //buf = sr.ReadLine();
 
             int NUnit = Convert.ToInt32(sr.ReadLine(), 10);
             for (int _i = 0; _i < NUnit; _i++)
             {
                 buf = sr.ReadLine();
-                if (buf == "Swordsman")
+                if (buf == "Construction")
                 {
                     buf = sr.ReadLine();
-                    Unit.CreateUnit(MapToSave.UnitPrefabArray[0], Unit.UnitType.Swordsman, Convert.ToInt32(sr.ReadLine(), 10), Convert.ToInt32(sr.ReadLine(), 10), MapToSave.UnitList, buf,1);
+                    if (buf == "TownHall")
+                    {
+                        buf = sr.ReadLine();
+                        Construction.CreateConstruction(MapToSave.UnitPrefabArray[4], Construction.ConstructionType.TownHall, Convert.ToInt32(sr.ReadLine(), 10), Convert.ToInt32(sr.ReadLine(), 10), MapToSave.UnitList, buf, Convert.ToInt32(sr.ReadLine(), 10));
+                    }
+                    if (buf == "Barracks")
+                    {
+                        buf = sr.ReadLine();
+                        Construction.CreateConstruction(MapToSave.UnitPrefabArray[5], Construction.ConstructionType.Barracks, Convert.ToInt32(sr.ReadLine(), 10), Convert.ToInt32(sr.ReadLine(), 10), MapToSave.UnitList, buf, Convert.ToInt32(sr.ReadLine(), 10));
+                    }
+                    if (buf == "Pit")
+                    {
+                        buf = sr.ReadLine();
+                        Construction.CreateConstruction(MapToSave.UnitPrefabArray[6], Construction.ConstructionType.Pit, Convert.ToInt32(sr.ReadLine(), 10), Convert.ToInt32(sr.ReadLine(), 10), MapToSave.UnitList, buf, Convert.ToInt32(sr.ReadLine(), 10));
+                    }
+                    if (buf == "Sawmill")
+                    {
+                        buf = sr.ReadLine();
+                        Construction.CreateConstruction(MapToSave.UnitPrefabArray[7], Construction.ConstructionType.Sawmill, Convert.ToInt32(sr.ReadLine(), 10), Convert.ToInt32(sr.ReadLine(), 10), MapToSave.UnitList, buf, Convert.ToInt32(sr.ReadLine(), 10));
+                    }
+                    ConstructionUnit = GameObject.Find(buf).GetComponent<Construction>();
+                    ConstructionUnit.Hitpoints = Convert.ToSingle(sr.ReadLine());
+                    ConstructionUnit.Damage = Convert.ToSingle(sr.ReadLine());
+                    ConstructionUnit.CurrentNumberActionPoints = Convert.ToInt32(sr.ReadLine());
+                    ConstructionUnit.StandardNumberActionPoints = Convert.ToInt32(sr.ReadLine());
                 }
-                if (buf == "Archer")
+                if (buf == "Unit")
                 {
                     buf = sr.ReadLine();
-                    Unit.CreateUnit(MapToSave.UnitPrefabArray[0], Unit.UnitType.Archer, Convert.ToInt32(sr.ReadLine(), 10), Convert.ToInt32(sr.ReadLine(), 10), MapToSave.UnitList, buf,1);
+                    if (buf == "Swordsman")
+                    {
+                        buf = sr.ReadLine();
+                        Unit.CreateUnit(MapToSave.UnitPrefabArray[0], Unit.UnitType.Swordsman, Convert.ToInt32(sr.ReadLine(), 10), Convert.ToInt32(sr.ReadLine(), 10), MapToSave.UnitList, buf, 1);
+                    }
+                    if (buf == "Archer")
+                    {
+                        buf = sr.ReadLine();
+                        Unit.CreateUnit(MapToSave.UnitPrefabArray[1], Unit.UnitType.Archer, Convert.ToInt32(sr.ReadLine(), 10), Convert.ToInt32(sr.ReadLine(), 10), MapToSave.UnitList, buf, 1);
+                    }
+                    if (buf == "Mage")
+                    {
+                        buf = sr.ReadLine();
+                        Unit.CreateUnit(MapToSave.UnitPrefabArray[2], Unit.UnitType.Mage, Convert.ToInt32(sr.ReadLine(), 10), Convert.ToInt32(sr.ReadLine(), 10), MapToSave.UnitList, buf, 1);
+                    }
+                    if (buf == "Killer")
+                    {
+                        buf = sr.ReadLine();
+                        Unit.CreateUnit(MapToSave.UnitPrefabArray[3], Unit.UnitType.Killer, Convert.ToInt32(sr.ReadLine(), 10), Convert.ToInt32(sr.ReadLine(), 10), MapToSave.UnitList, buf, 1);
+                    }
+                    UnitBufer = GameObject.Find(buf).GetComponent<Unit>();
+                    UnitBufer.Hitpoints = Convert.ToSingle(sr.ReadLine());
+                    UnitBufer.Damage = Convert.ToSingle(sr.ReadLine());
+                    UnitBufer.CurrentNumberActionPoints = Convert.ToInt32(sr.ReadLine());
+                    UnitBufer.StandardNumberActionPoints = Convert.ToInt32(sr.ReadLine()); 
                 }
-                if (buf == "Mage")
-                {
-                    buf = sr.ReadLine();
-                    Unit.CreateUnit(MapToSave.UnitPrefabArray[0], Unit.UnitType.Mage, Convert.ToInt32(sr.ReadLine(), 10), Convert.ToInt32(sr.ReadLine(), 10), MapToSave.UnitList, buf,1);
-                }
-                if (buf == "Killer")
-                {
-                    buf = sr.ReadLine();
-                    Unit.CreateUnit(MapToSave.UnitPrefabArray[0], Unit.UnitType.Killer, Convert.ToInt32(sr.ReadLine(), 10), Convert.ToInt32(sr.ReadLine(), 10), MapToSave.UnitList, buf,1);
-                }
-                UnitBufer = GameObject.Find(buf).GetComponent<Unit>();
-                UnitBufer.Hitpoints = Convert.ToSingle(sr.ReadLine());
-                UnitBufer.Damage = Convert.ToSingle(sr.ReadLine());
-                UnitBufer.CurrentNumberActionPoints = Convert.ToInt32(sr.ReadLine());
-                UnitBufer.StandardNumberActionPoints = Convert.ToInt32(sr.ReadLine());
             }
             sr.Close();
         }
