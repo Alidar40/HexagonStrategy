acba153d2cca5b3c83c9aa27f28382bcb808f81e
Andrey Eltsov <andrei.eltzov@gmail.com>
2017-07-14 11:05:16 +0300

    Изменены спрайты игрового окружения. Добавлена кнпока отмены в меню выбора карты. Добавлена кнопка включения/отключения звука на основной игровой сцене.
    Изменены спрайты игрового окружения. Добавлена кнпока отмены в меню выбора карты. Добавлена кнопка включения/отключения звука на основной игровой сцене.

---
+++ Strategy/Assets/Scripts/BackToMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackToMenu : MonoBehaviour {
+
+    Menu menu;
+
+    void Start () {
+        menu = GameObject.Find("Menu").GetComponent<Menu>();
+    }
+
+    public void ClickOnCancel()
+    {
+        menu.OpenMenu();
+        Destroy(menu.gameObject);
+    }
+}
